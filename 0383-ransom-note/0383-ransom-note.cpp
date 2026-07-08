10cabaa6f0b5abf201717e4e26d46cfdd25e20a7
VIshal <79686922+wearetec9@users.noreply.github.com>
2026-07-08 22:44:44 +0530

    Time: 2 ms (59.54%), Space: 12.2 MB (11.4%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        std::unordered_map<char,int> mp ;
+        std::unordered_map<char,int> mp2 ;
+        // if(ransomNote.length() != magazine.length() ) return false; 
+        for(char c : ransomNote){
+            mp[c]++;
+        }
+        for(char c :  magazine ){
+            mp2[c]++;
+        }
+        for(auto const& it : mp){
+            if(mp2[it.first] < it.second) return false ; 
+        }
+
+        return true; 
+    }
+};
